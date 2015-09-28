8af5aa621667b3e81767291c4c1c3769cdb1c436
gandalf23 <gandalf23@rocketmail.com>
2015-09-28 19:11:07 +0000

    small changes

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -13,6 +13,7 @@ class LeaguesController < ApplicationController
   def show
     if !user_drafted?
       @team = Team.new
+      @team.name = current_user.name + @league.name
       @team.league_id = @league.id
       @team.save
       redirect_to team_path(id: @team.id)
