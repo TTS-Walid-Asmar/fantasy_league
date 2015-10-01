a6fadc4ec03b0c6f18842a773322012e370ab60a
Jeffrey Goodall <jgoodall628@gmail.com>
2015-10-01 08:06:30 -0500

    fixed rake

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,36 +1,37 @@
 class League < ActiveRecord::Base
 
-    belongs_to :fantasy_stat
-    has_many :teams
-    has_many :leagues_users
-    has_many :users, through: :leagues_users
-    serialize :games, Array
-    serialize :player_list, Array
+  belongs_to :fantasy_stat
+  has_many :teams
+  has_many :leagues_users
+  has_many :users, through: :leagues_users
+  serialize :games, Array
+  serialize :player_list, Array
 
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
 
-      def slug_candidates
-      [
-        :name,
-        [:name, :id],
-      ]
-    end
+  def slug_candidates
+    [
+      :name,
+      [:name, :id],
+    ]
+  end
 
-    def upcoming?
-      self.status == 'Upcoming'
-    end
-    def prizes
-      total = 0.95*cost*max_participants
-    end
-    def money_for_payout
-      total = 0.95*self.users.size*self.cost
-    end
-    def rank_users_by_score
-      users.sort{|a, b| b.league_score(self) <=> a.league_score(self)}
-    end
-    def process_league
-      rankings = rank_users_by_score
+  def upcoming?
+    self.status == 'Upcoming'
+  end
+  def prizes
+    total = 0.95*cost*max_participants
+  end
+  def money_for_payout
+    total = 0.95*self.users.size*self.cost
+  end
+  def rank_users_by_score
+    users.sort{|a, b| b.league_score(self) <=> a.league_score(self)}
+  end
+  def process_league
+    rankings = rank_users_by_score
+    if rankings.size !=0
       winning_score = rankings[((rankings.size-1)/2).to_i].league_score(self)
       winners = rankings.select{|user| user.league_score(self) >= winning_score}
       payout = money_for_payout/(winners.size)
@@ -39,12 +40,13 @@ class League < ActiveRecord::Base
         winner.balance += payout
         winner.save
       end
-      self.status = "Past"
-      self.save
     end
-    def is_full?
-      self.users.size >= self.max_participants
-    end
+    self.status = "Past"
+    self.save
+  end
+  def is_full?
+    self.users.size >= self.max_participants
+  end
 
 
 
